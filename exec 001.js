f6dcbd034a22aa36ee420465d109e670abd851e9
Secretaria <jiomar213@gmail.com>
2022-12-14 13:45:58 -0300

    Inicio curso JS

---
+++ exec 001.js
@@ -0,0 +1,15 @@
+var n1 = 5
+var n2 = 2.61
+nome = 'jiomarlison'
+
+console.log('n1 + n2 = ', n1+n2)
+console.log('n1 - n2 = ', n1-n2)
+console.log('n1 * n2 = ', n1*n2)
+console.log('n1 / n2 = ', n1/n2)
+console.log('n1 ** n2 = ', n1 ** n2)
+
+console.log('Tipo n1: ', typeof(n1))
+console.log('Tipo n2: ', typeof(n2))
+console.log('Tipo nome: ', typeof(nome))
+console.log('Tipo booleano(true): ', typeof(true))
+console.log('Tipo booleano(false): ', typeof(false))
